a05e51f30f6069c796cb5a437dd675f0ecd8c6ee
ymouafak <ymouafak@e2r3p12.1337.ma>
2026-03-29 17:27:13 +0100

    done

---
+++ ex3/functools_artifacts.py
@@ -0,0 +1,71 @@
+from functools import reduce, partial, lru_cache, singledispatch
+from operator import add, mul
+
+
+def spell_reducer(spells: list[int], operation: str) -> int:
+    result = None
+
+    ops = {
+            "add":      add,
+            "multiply": mul,
+            "max":      max,
+            "min":      min,
+        }
+    if operation in ops:
+        result = reduce(ops[operation], spells)
+    return result
+
+
+def partial_enchanter(base_enchantment: callable) -> dict[str, callable]:
+    fire = partial(base_enchantment, 50, "fire")
+    ice = partial(base_enchantment, 50, "ice")
+    lightning = partial(base_enchantment, 50, "lightning")
+
+    return {'fire_enchant': fire,
+            'ice_enchant': ice,
+            'lightning_enchant': lightning}
+
+
+@lru_cache()
+def memoized_fibonacci(n: int) -> int:
+    if n < 2:
+        return n
+
+    return memoized_fibonacci(n - 1) + memoized_fibonacci(n - 2)
+
+
+def spell_dispatcher() -> callable:
+
+    @singledispatch
+    def dispatcher_function(value):
+        ...
+
+    @dispatcher_function.register
+    def _(value: int):
+        return f'deal {value} damage'
+
+    @dispatcher_function.register
+    def _(value: str):
+        return value
+
+    @dispatcher_function.register
+    def _(value: list):
+        results = [dispatcher_function(spell) for spell in value]
+        return results
+
+    return dispatcher_function
+
+
+def main():
+
+    print("\nTesting spell reducer...")
+    print(f"Sum: {spell_reducer([25, 25, 25, 25], 'add')}")
+    print(f"Product: {spell_reducer([80, 30, 100], 'multiply')}")
+    print(f"Max: {spell_reducer([25, -1, 40], 'max')}")
+
+    print("\nTesting memoized fibonacci...")
+    print(f"Fib(10): {memoized_fibonacci(10)}")
+    print(f"Fib(15): {memoized_fibonacci(15)}")
+
+
+main()
---
+++ ex4/decorator_mastery.py
@@ -0,0 +1,75 @@
+from functools import wraps
+import time
+
+
+def spell_timer(func: callable) -> callable:
+    @wraps(func)
+    def casting():
+        start = time.time()
+        print(f"Casting {func.__name__}...")
+        result = func()
+        end = time.time()
+        print(f"Spell completed in {(end - start):.6f} seconds")
+        return result
+
+    return casting
+
+
+def power_validator(min_power: int) -> callable:
+    def validate(func: callable) -> callable:
+        @wraps(func)
+        def validate_inner(self, spell_name, power, *args,
+                           **kwargs) -> str | None:
+            if power >= min_power:
+                return func(self, spell_name, power, *args, **kwargs)
+            else:
+                return "Insufficient power for this spell"
+        return validate_inner
+    return validate
+
+
+def retry_spell(max_attempts: int) -> callable:
+    def retry(func: callable) -> callable:
+        def inner(*args, **kwargs) -> str | None:
+            for i in range(1, max_attempts + 1):
+                try:
+                    res = func(*args, **kwargs)
+                    return res
+                except Exception:
+                    print(f"Spell failed, retrying... ({i}/{max_attempts})")
+            return f"Spell casting failed after {max_attempts} attempts"
+        return inner
+    return retry
+
+
+class MageGuild:
+    @staticmethod
+    def validate_mage_name(name: str) -> bool:
+        if len(name) < 3 or \
+                not all([c.isalpha() or c.isspace() for c in name]):
+            return False
+        return True
+
+    @power_validator(min_power=10)
+    def cast_spell(self, spell_name: str, power: int) -> str:
+        return f"Successfully cast {spell_name} with {power} power"
+
+
+def fireball():
+    return "Fireball cast!"
+
+
+def main():
+    print("\nTesting spell timer...")
+    func = spell_timer(fireball)
+    print(f"Result: {func()}")
+
+    print("\nTesting MageGuild...")
+    mage_guild = MageGuild()
+    print(mage_guild.validate_mage_name("Magic"))
+    print(mage_guild.validate_mage_name("Magic1"))
+    print(mage_guild.cast_spell("Lightning", 15))
+    print(mage_guild.cast_spell("Lightning", 5))
+
+
+main()
